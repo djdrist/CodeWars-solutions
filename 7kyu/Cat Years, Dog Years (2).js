9cb86b3235af6259f1e33190a39cc29fad097dc9
Lukasz Kallas <lukasz.kallas@gmail.com>
2023-04-28 16:14:11 +0200

    28.04.2023

---
+++ 7kyu/Cat Years, Dog Years (2).js
@@ -0,0 +1,31 @@
+/*
+This is related to my other Kata about cats and dogs.
+
+Kata Task
+
+I have a cat and a dog which I got as kitten / puppy.
+
+I forget when that was, but I do know their current ages as catYears and dogYears.
+
+Find how long I have owned each of my pets and return as a list [ownedCat, ownedDog]
+
+NOTES:
+
+Results are truncated whole numbers of "human" years
+Cat Years
+
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+*/
+
+var ownedCatAndDog = function (catYears, dogYears) {
+	const cat = catYears < 15 ? 0 : catYears < 24 ? 1 : 2 + (catYears - 24) / 4;
+	const dog = dogYears < 15 ? 0 : dogYears < 24 ? 1 : 2 + (dogYears - 24) / 5;
+	return [Math.floor(cat), Math.floor(dog)];
+};
