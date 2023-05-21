519414f40b7365358a9f3d0e5abe0d9cecd3e132
Lukasz Kallas <lukasz.kallas@gmail.com>
2023-05-21 10:29:43 +0200

    21.05.2023

---
+++ 7kyu/Slaphead.js
@@ -0,0 +1,36 @@
+/*
+Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
+
+You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
+
+You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
+
+0 hairs --> "Clean!"
+1 hair --> "Unicorn!"
+2 hairs --> "Homer!"
+3-5 hairs --> "Careless!"
+>5 hairs --> "Hobo!"
+
+So for this head: "------/------" you shoud return:
+
+["-------------", "Unicorn"]
+*/
+
+function bald(x) {
+	const count = x.split('/').length - 1;
+	const result = x.replace(/\//g, '-');
+	switch (count) {
+		case 0:
+			return [result, 'Clean!'];
+		case 1:
+			return [result, 'Unicorn!'];
+		case 2:
+			return [result, 'Homer!'];
+		case 3:
+		case 4:
+		case 5:
+			return [result, 'Careless!'];
+		default:
+			return [result, 'Hobo!'];
+	}
+}
